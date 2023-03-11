a3ced080a865b4ab59ad3499b1c46ca16fb3db65
George Kobunov <ancient.gekob@gmail.com>
2023-03-11 14:14:25 +0300

    Some wrapper fixes

--- src/app/ui/Wrapper/Wrapper.tsx
+++ src/app/ui/Wrapper/Wrapper.tsx
@@ -4,11 +4,14 @@ export const Wrapper = styled("div", {
   marginLeft: "auto",
   marginRight: "auto",
   maxWidth: 1300,
-  paddingLeft: '1rem',
-  paddingRight: '1rem',
-  '@media (max-width: 400px)': {
-    paddingLeft: '0.5rem',
-    paddingRight: '0.5rem',
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
+  "@media (max-width: 400px)": {
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
   },
-  paddingTop: '2rem'
+  paddingTop: "2rem",
+  display: "flex",
+  flexDirection: "column",
+  height: '100%',
 });
