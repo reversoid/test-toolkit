7b3c0e9f64acaaccaba9d2c9466aa087806b264a
George Kobunov <ancient.gekob@gmail.com>
2023-03-11 14:16:35 +0300

    Make layout prettier

--- src/app/ui/Wrapper/Wrapper.tsx
+++ src/app/ui/Wrapper/Wrapper.tsx
@@ -4,14 +4,12 @@ export const Wrapper = styled("div", {
   marginLeft: "auto",
   marginRight: "auto",
   maxWidth: 1300,
-  paddingLeft: "1rem",
-  paddingRight: "1rem",
   "@media (max-width: 400px)": {
     paddingLeft: "0.5rem",
     paddingRight: "0.5rem",
   },
-  paddingTop: "2rem",
   display: "flex",
   flexDirection: "column",
   height: '100%',
+  padding: '2rem 1rem'
 });
--- src/pages/Repository/SearchPage/ui/PaginatorContainer.tsx
+++ src/pages/Repository/SearchPage/ui/PaginatorContainer.tsx
@@ -2,4 +2,8 @@ import { styled } from "@stitches/react";
 
 export const PaginatorContainer = styled("div", {
   marginTop: "2rem",
+  flexGrow: "2",
+  display: "flex",
+  justifyContent: "flex-end",
+  flexDirection: 'column'
 });
