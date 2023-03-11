fd78621ca73b944057d1faaa30946a8b746a1ed6
George Kobunov <ancient.gekob@gmail.com>
2023-03-11 13:53:18 +0300

    Create repository page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
+import RepositoryPage from "../pages/Repository/RepositoryPage/RepositoryPage";
 import SearchPage from "../pages/Repository/SearchPage/ui/SearchPage";
 import { Wrapper } from "./ui/Wrapper/Wrapper";
 
 function App() {
   return (
     <Wrapper>
-      <SearchPage />
+      <RepositoryPage />
     </Wrapper>
   );
 }
--- src/pages/Repository/RepositoryPage/RepositoryPage.tsx
+++ src/pages/Repository/RepositoryPage/RepositoryPage.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 import { Input } from "../../../shared/ui/Input/Input";
+import { styled } from "@stitches/react";
+import { Link } from "../../../shared/ui/Link/Link";
+
+const Title = styled("h1", {
+  fontSize: "3rem",
+  marginBottom: "1.25rem",
+});
+
+const Property = styled("p");
+
+const Img = styled("img", {
+  width: "5rem",
+  height: "5rem",
+  objectFit: "cover",
+  borderRadius: '50%',
+  border: '2px solid white'
+});
+
+const PageContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+});
 
 const RepositoryPage = () => {
   return (
     <>
-      <Input type="text" />
+      <Title>moofy-frontend</Title>
+      <PageContainer>
+        <Property>Stars: 10</Property>
+        <Property>Last commit cate: 10.10.2020</Property>
+        <Property>
+          Creator: <Link href="https://google.com">reversoid</Link>
+        </Property>
+        <Property>languages: Typescript, React</Property>
+        <Property>Short description: Hello there!</Property>
+        <Img src="https://avatars.mds.yandex.net/i?id=b14bd3bcf8f1ca20e0acccdbd5867731ac836f80-8497350-images-thumbs&n=13" />
+      </PageContainer>
     </>
   );
 };
