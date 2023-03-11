7492004dc70727e870f3a942c3bd3b60ab8dfc18
George Kobunov <ancient.gekob@gmail.com>
2023-03-11 14:54:05 +0300

    Try set github access token

--- src/app/api/apolloClient.ts
+++ src/app/api/apolloClient.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const githubAccessToken = "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";
+const githubAccessToken = "g hp_ay PSr0 rpC Pbr0GA pdJQupon DE gp Sqo 4eml 1g";
 
 export const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `bearer ${githubAccessToken}`,
+    Authorization: `bearer ${githubAccessToken.replace(/ /g, "")}`,
   },
 });
